151f6d138f61f80bf343fcf939d37566aef45934
jcpsantos <juancaiops@gmail.com>
2018-05-27 00:46:35 -0300

    Tratamento de erros

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -1,15 +1,17 @@
+import { ErrorHandler } from './../app.error-handler';
 import { BURGUER_API } from './../app.api';
 import { Restaurante } from './restaurante/restaurante.model';
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RestaurantsService{
     constructor(private http: Http){}
 
     restaurants(): Observable<Restaurante[]>{
-      return this.http.get(`${BURGUER_API}/restaurants`).map(response => response.json())
+      return this.http.get(`${BURGUER_API}/restaurants`).map(response => response.json()).catch(ErrorHandler.handleError)
     }
 }
