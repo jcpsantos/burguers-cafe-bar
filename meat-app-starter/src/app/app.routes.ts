f1499d026d25d049240ee9eff76377840477e7a9
jcpsantos <juancaiops@gmail.com>
2018-07-02 00:27:42 -0300

    Carregamento tardio do modulo Sobre(About)

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -3,14 +3,13 @@ import { OrderComponent } from './order/order.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from "@angular/router";
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
+    {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'restaurants', component: RestaurantsComponent},
     {path: 'restaurants/:id', component:RestaurantDetailComponent,
         children: [
